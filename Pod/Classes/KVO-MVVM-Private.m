97a5e4e1070460c2812607de2d892773ded6988f
Anton Bukov <k06aaa@gmail.com>
2016-08-30 22:26:22 +0300

    Fixed copyrights and license

--- Pod/Classes/KVO-MVVM-Private.m
+++ Pod/Classes/KVO-MVVM-Private.m
@@ -1,9 +1,20 @@
 //
-//  KVO-MVVM-Unobserver.m
-//  Pods
+//  KVO-MVVM-Private.m
+//  KVO-MVVM
 //
-//  Created by Anton Bukov on 30.08.16.
+//  Copyright (c) 2016 Machine Learning Works
 //
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <objc/runtime.h>
--- Pod/Classes/KVO-MVVM.h
+++ Pod/Classes/KVO-MVVM.h
@@ -2,8 +2,19 @@
 //  KVO-MVVM.h
 //  KVO-MVVM
 //
-//  Created by Anton Bukov on 16.03.16.
+//  Copyright (c) 2016 Machine Learning Works
 //
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
@@ -12,10 +23,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSObject (MLWKVOMVVM)
 
-- (void)mvvm_observe:(NSString *)keyPath with:(void (^)(id self, id value))block;
-- (void)mvvm_observe:(NSString *)keyPath options:(NSKeyValueObservingOptions)options with:(void (^)(id self, id value))block;
-- (void)mvvm_observeCollection:(NSString *)keyPath with:(void (^)(id self, id value, NSKeyValueChange change, NSIndexSet *indexes))block;
-- (void)mvvm_observeCollection:(NSString *)keyPath options:(NSKeyValueObservingOptions)options with:(void (^)(id self, id value, NSKeyValueChange change, NSIndexSet *indexes))block;
+- (void)mvvm_observe:(NSString *)keyPath with:(void (^)(id self, id _Nullable value))block;
+- (void)mvvm_observe:(NSString *)keyPath options:(NSKeyValueObservingOptions)options with:(void (^)(id self, id _Nullable value))block;
+- (void)mvvm_observeCollection:(NSString *)keyPath with:(void (^)(id self, id _Nullable value, NSKeyValueChange change, NSIndexSet *indexes))block;
+- (void)mvvm_observeCollection:(NSString *)keyPath options:(NSKeyValueObservingOptions)options with:(void (^)(id self, id _Nullable value, NSKeyValueChange change, NSIndexSet *indexes))block;
 
 - (void)mvvm_unobserve:(NSString *)keyPath;
 - (void)mvvm_unobserveAll;
